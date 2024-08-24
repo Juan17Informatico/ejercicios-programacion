f13340131932e5ba0ad183dabd4c1057bce8c467
JuanCM <juancampuzano2356@gmail.com>
2024-08-24 11:55:35 -0500

    Ejercicio en proceso

---
+++ 03-fibonacci/app.js
@@ -0,0 +1,28 @@
+/*
+ * * LA SUCESIÓN DE FIBONACCI *
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+
+const sucesionFibonacci = ( cantidadNumeros ) => {
+
+    if ( cantidadNumeros < 0 ) return false;
+
+    let a = 0; 
+    let b = 1; 
+
+    for (let i = 0; i < cantidadNumeros; i++) {
+        
+        console.log(j);
+        j = (i + j); 
+        
+
+    }
+
+}
+
+sucesionFibonacci( 50 );
