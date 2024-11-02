98cd8f7dcbe0eb271d65b00e3a751df8ccdedf32
JuanCM <juancampuzano2356@gmail.com>
2024-11-02 11:50:59 -0500

    Inicio del ejercicio 12

---
+++ 12-eliminando-caracteres/app.js
@@ -0,0 +1,24 @@
+/*
+ * Crea una función que reciba dos cadenas como parámetro (str1, str2)
+ * e imprima otras dos cadenas como salida (out1, out2).
+ * - out1 contendrá todos los caracteres presentes en la str1 pero NO
+ *   estén presentes en str2.
+ * - out2 contendrá todos los caracteres presentes en la str2 pero NO
+ *   estén presentes en str1.
+ */
+
+/**
+ * Función que recibe dos cadenas de texto como entrada y genera dos nuevas cadenas como salida
+ * - La primera cadena de salida (`out1`) contiene los caracteres presentes en `str1` pero no en `str2`.
+ * - La segunda cadena de salida (`out2`) contiene los caracteres presentes en `str2` pero no en `str1`.
+ * @param {String} str1 Cadena de texto 1
+ * @param {String} str2 Cadena de texto 2
+ * @returns {{ out1: string, out2: string }} Un objeto con dos propiedades:
+ *   - `out1`: Cadena con los caracteres exclusivos de `str1`.
+ *   - `out2`: Cadena con los caracteres exclusivos de `str2`.
+ */
+const eliminandoCaracteres = ( str1, str2 ) => {
+
+ 
+
+}
