87bc2d30a742bb4f586d5354e8451aa1607a3749
JuanCM <juancampuzano2356@gmail.com>
2024-11-30 10:26:08 -0500

    Ejercicio 14 Finalizado

--- 14-factorial-recursivo/app.js
+++ 14-factorial-recursivo/app.js
@@ -2,3 +2,14 @@
  * Escribe una función que calcule y retorne el factorial de un número dado
  * de forma recursiva.
  */
+
+const factorial = (n) => {
+    
+    if ( n <= 0 ) return 1; 
+
+    return  n * factorial( n - 1 );
+
+}
+
+console.log(factorial(3));
+console.log(factorial(10));
