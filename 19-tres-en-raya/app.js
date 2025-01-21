ebec6e8e46daf27d301fbd1c0e0705bd10afc945
JuanCM <juancampuzano2356@gmail.com>
2025-01-21 17:12:07 -0500

    Ejercicio 19 - Se agregan validaciones a la función. Se recorre array y se crea array de combinaciones

--- 19-tres-en-raya/app.js
+++ 19-tres-en-raya/app.js
@@ -15,7 +15,59 @@
  * @param {Array} matriz del juego por el usuario
  */
 const tresEnRaya = (matriz) => {
- 
-    if ( Array.isArray(matriz) ) {}
-    
-}
+
+    if ((matriz[0].length !== 3) || (matriz[1].length !== 3) || (matriz[2].length !== 3)) 
+        return "Nulo";
+
+    const arrayDeResultado = [ ...matriz[0], ...matriz[1], ...matriz[2] ];
+
+    const combinacionesGanadoras = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    const resultado = [];
+
+    combinacionesGanadoras.forEach((combinacion) => {
+        const [ a, b, c ] = combinacion;
+        
+        console.log(arrayDeResultado[a], arrayDeResultado[b],
+            arrayDeResultado[c]);
+        
+
+        if( 
+            arrayDeResultado[a] !== " " &&
+            arrayDeResultado[a] === arrayDeResultado[b] &&
+            arrayDeResultado[a] === arrayDeResultado[c]
+        ){
+            resultado.push(arrayDeResultado[a]);
+        }
+
+    });
+
+    return resultado;
+
+};
+
+console.log(
+    "Gana O:" +
+    tresEnRaya([
+        ["x", "x", "o"],
+        ["x", "o", " "],
+        ["o", " ", " "],
+    ])
+);
+console.log(
+    "Gana x:" +
+    tresEnRaya([
+        ["x", "x", "o"],
+        ["x", "o", "o"],
+        ["o", "o", "x"],
+    ])
+);
